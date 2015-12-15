eb7afa39ced25d0dc913e1f7b8db290a0c204662
mihasic <mihasic@gmail.com>
2015-12-15 16:23:14 +0100

    Initial commit of the analyzer.

---
+++ src/DateTimeNowAnalyzer/DiagnosticAnalyzer.cs
@@ -0,0 +1,64 @@
+namespace DateTimeNowAnalyzer
+{
+    using System.Collections.Immutable;
+    using System.Linq;
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp;
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
+    using Microsoft.CodeAnalysis.Diagnostics;
+
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class DateTimeNowAnalyzerAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "DateTimeNowAnalyzer";
+        private const string Category = "Naming";
+
+        private static readonly string[] InvalidTokens =
+        {
+            "System.DateTime.Now",
+            "System.DateTime.UtcNow",
+            "System.DateTimeOffset.Now",
+            "System.DateTimeOffset.UtcNow"
+        };
+
+        // https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Localizing%20Analyzers.md
+        private static readonly LocalizableString Title = new LocalizableResourceString(
+            nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof (Resources));
+
+        private static readonly LocalizableString MessageFormat =
+            new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager,
+                typeof (Resources));
+
+        private static readonly LocalizableString Description =
+            new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager,
+                typeof (Resources));
+
+        private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat,
+            Category,
+            DiagnosticSeverity.Error, true, Description);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            // https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Analyzer%20Actions%20Semantics.md
+            context.RegisterSyntaxNodeAction(AnalyzeSyntaxNode, SyntaxKind.SimpleMemberAccessExpression);
+        }
+
+        private void AnalyzeSyntaxNode(SyntaxNodeAnalysisContext context)
+        {
+            var memberAccessExpr = (MemberAccessExpressionSyntax) context.Node;
+            var name = memberAccessExpr.Name.ToString();
+            if (name == "Now" || name == "UtcNow")
+            {
+                var symbol = context.SemanticModel.GetSymbolInfo(memberAccessExpr).Symbol as IPropertySymbol;
+                var symbolText = symbol?.ToString();
+                if (InvalidTokens.Contains(symbolText))
+                {
+                    var diagnostic = Diagnostic.Create(Rule, memberAccessExpr.GetLocation(), Description);
+                    context.ReportDiagnostic(diagnostic);
+                }
+            }
+        }
+    }
+}
