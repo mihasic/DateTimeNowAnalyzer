866c5ff17069bf99025f2696991c2d7b66920b63
Mykhailo Rybnikov <mihasic@gmail.com>
2017-03-09 17:05:00 +0100

    set environment so it works

---
+++ sample/sample/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using static System.DateTime;
+
+namespace sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = Now;
+            x = DateTime.Now;
+        }
+    }
+}
